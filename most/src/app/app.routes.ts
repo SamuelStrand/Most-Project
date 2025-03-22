eacaa055b990fabc87b2262423585ecd9de8a991
dayana <nnameal@bk.ru>
2025-03-22 23:15:55 +0500

    resume

--- most/src/app/app.routes.ts
+++ most/src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { VacancyPageComponent } from './vacancy-page/vacancy-page.component';
+import { ResumeComponent } from './resume/resume.component';
 
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'about', component: ResumeComponent },
   {path: 'search/:searchTerm', component:HomeComponent},
   {path: 'vacancy/:id', component: VacancyPageComponent}
 
